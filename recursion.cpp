36dee069b771bd502c9c81e861c9031a26b0439d
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-08-04 00:29:20 +0530

    .

--- recursion.cpp
+++ recursion.cpp
@@ -127,13 +127,15 @@ void replacePi (const string &s) {
     }
 }
 
-void towerOfHanoi (int n, char src, char dest, char help) {
-    // we have to move n discs from src to dest using help. For that we move the top n-1 discs from src to help using dest as helping rod and then move the nth disc (biggest one) from source to dest using helping rod and then move the other n-1 discs to dest using using src as help
-    if (n == 0) 
+void towerOfHanoi (int n, char A, char B, char C) {
+    // we have to move n discs from A (source) to C (dest) using C (help). For that we move the top n-1 discs from src to help using dest as helping rod and then move the nth disc (biggest one) from source to dest using helping rod and then move the other n-1 discs to dest using using src as help
+    if (n == 1) {
+        cout << "Move 1 from " << A << " to " << C << endl; 
         return;
-    towerOfHanoi(n-1, src, help, dest);
-    cout << "Move from " << src << " to " << dest << endl;
-    towerOfHanoi(n-1, help, dest, src);
+    }
+    towerOfHanoi(n-1, A, C, B);
+    cout << "Move " << n << " from " << A << " to " << C << endl;
+    towerOfHanoi(n-1, B, A, C);
 }
 
 // my way
@@ -148,7 +150,7 @@ void towerOfHanoi (int n, char src, char dest, char help) {
 } */
 
 // we will send the whole string except the first character to the recursive function assuming that the function will return the string removing all duplicates then we will compare the first char to the first character of the returned string and if they are equal then we will only return the returned string otherwise we will return char + returned string
-/* string removeDuplicates (const string &s) {
+string removeDuplicates (const string &s) {
     if (s.length() == 0)
         return "";
     char ch = s[0];
@@ -157,7 +159,7 @@ void towerOfHanoi (int n, char src, char dest, char help) {
         return ans;
     return ch+ans;
 }
- */
+
 string moveXtoLast (const string &s) {
     // we will be given a string and we have to move all the x in it to last position.
     // similar thought process like the last question we will compare first char and the remaining string and if first char is x we will add it to last and return
@@ -168,6 +170,30 @@ string moveXtoLast (const string &s) {
     if (ch == 'x')
         return ans + ch;
     return ch + ans;
+}
+
+void subStrings (string s, string ans) {
+    // here s is our string and ans is ""
+    // we have to write all substrings using recursion. Basically we start with an empty string and at each step we have two steps either to add a char or to not till we run out of characters in original string. 
+    // Ex we have "" and string is "ABC" then at first step we can add "A" or not then at next step we can add "B" to "A" or not and add "B" to "" or not then at final step we can add or not add "C" to "AB", "A", "B", "" resulting in 8 sub strings
+    if (s.length() == 0) {
+        cout << ans << endl;
+        return;
+    }
+    subStrings(s.substr(1), ans);  // not add
+    subStrings(s.substr(1), ans+s[0]);    // add
+
+}
+
+void subStringAscii (string s, string ans) {
+    // similar to prev ques but here we have to add or not add ascii value of char we are adding or not adding so we will just call it 3 times
+    if (s.length() == 0) {
+        cout << ans << endl;
+        return;
+    }
+    subStringAscii(s.substr(1), ans);  // not add
+    subStringAscii(s.substr(1), ans + s[0]);    // add
+    subStringAscii(s.substr(1), ans + to_string((int)s[0]));        
 }
 
 int main () {
@@ -182,12 +208,15 @@ int main () {
     //int arr[] = {1, 2, 3, 2, 5, 2, 3};
     //cout << firstOccurance(arr, 7, 2, 0) << endl;
     //cout << lastOccurance(arr, 7, 2, 6) << endl;
-    string s;
-    getline(cin, s);
+    // string s;
+    // getline(cin, s);
     // reverse(s, 0, s.length());
     // cout << s << endl;
     //replacePi(s);
-    //towerOfHanoi(3, 'A', 'C', 'B');
-    cout << moveXtoLast(s) << endl;
+    towerOfHanoi(3, 'A', 'B', 'C');
+    //cout << moveXtoLast(s) << endl;
+    //string s, ans = "";
+    //cin >> s;
+    //subStringAscii(s, ans);
 
 }
