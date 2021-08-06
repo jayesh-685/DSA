90b9f28cec58948dea71a9293f843c8160ce0569
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-08-06 16:13:09 +0530

    insertEnd

--- linkedList.cpp
+++ linkedList.cpp
@@ -35,21 +35,35 @@ void recursionTraverse (Node *head) {
     recursionTraverse(head->next);
 }
 
-Node* insertAtStart (Node *head, int element) {
+Node* insertBegin (Node *head, int element) {
+    // to add an element at start of linked list
     Node *newHead = new Node (element);
     newHead->next = head;
     return newHead;
 }
 
+Node* insertEnd (Node *head, int element) {
+    if (head == NULL) {
+        Node *x = new Node (element);
+        return x;
+    }
+    Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = new Node (element);
+    return head;
+}
+
 int main() 
 { 
-    // // simple implementation
-    Node *head=new Node(10);
-	Node *temp1=new Node(20);
-	Node *temp2=new Node(30);
-	head->next=temp1;
-	temp1->next=temp2;
-	// cout<<head->data<<"-->"<<temp1->data<<"-->"<<temp2->data;
+    // simple implementation
+    // Node *head=new Node(10);
+	// Node *temp1=new Node(20);
+	// Node *temp2=new Node(30);
+	// head->next=temp1;
+	// temp1->next=temp2;
+    // traverse(head);
 
     //compact implementation for 3 nodes
     // Node *head = new Node (10);
@@ -59,6 +73,18 @@ int main()
     //recursionTraverse(head);    
     // even after using the traverse function we still get 10 20 30 from the next line because a copy of the pointer is made and the original pointer stays at the same position
     //cout << head->data << " " << head->next->data << " " << head->next->next->data << endl;
-    traverse(insertAtStart(head, 5));
+    // Node *head = NULL;
+    // head = insertBegin(head, 5);
+    // head = insertBegin(head, 10);
+    // head = insertBegin(head, 15);
+    // head = insertBegin(head, 20);
+    // traverse(head);
+
+    Node *head = NULL;
+    head = insertEnd(head, 10);
+    head = insertEnd(head, 20);
+    head = insertEnd(head, 30);
+    head = insertEnd(head, 40);
+    traverse(head);
     return 0;
 } 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,3 @@
-while (small != 0) {
-        int diff = big-small;
-        big = small;
-        small = diff;
-        cout << big << " " << small << " " << diff << endl;
-    }
+Node* insertEnd (Node *head, int element) {
+    
+}
