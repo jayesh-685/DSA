1c8a85cd9bbd6d6dde5452363a5d6ae4c20acd52
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-08-08 22:47:05 +0530

    DLL

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -19,6 +19,63 @@ void traverse (Node* head) {
     }
 }
 
+Node* insertBegin (Node* head, int data) {
+    Node* newNode = new Node (data);
+    if (head == NULL) {
+        return newNode;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    return newNode;
+}
+
+Node* insertEnd (Node* head, int data) {
+    Node* newNode = new Node (data);
+    if (head == NULL)
+        return newNode;
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+    return head;
+}
+
+// Node* reverseDLL (Node* head) {
+//     if (head == NULL)
+//         return head;
+//     int count = 1;
+//     Node* begin = head;
+//     Node* end = head;
+//     while (end->next != NULL) {
+//         count++;
+//         end = end->next;
+//     }
+//     for (int i=1; i<=count/2; i++) {
+//         int temp = begin->data;
+//         begin->data = end->data;
+//         end->data = temp;
+//         begin = begin->next;
+//         end = end->prev;
+//     }
+//     return head;
+// }
+
+Node* reverseDLL (Node* head) {
+    if (head == NULL || head->next == NULL)
+        return head;
+    Node* curr = head;
+    Node* temp = NULL;
+    while (curr != NULL) {
+        temp = curr->prev;
+        curr->prev = curr->next;
+        curr->next = temp;
+        curr = curr->prev;
+    }
+    return temp->prev;
+}
+
 int main () {
     // Node *head = new Node (10);
     // head->next = new Node (20);
@@ -26,12 +83,20 @@ int main () {
     // head->next->next = new Node (30);
     // head->next->next->prev = head->next;
     // traverse(head);
-    Node *head = new Node (10);
-    Node *temp1 = new Node (20);
-    Node *temp2 = new Node (30);
-    head->next = temp1;
-    temp1->prev = head;
-    temp1->next = temp2;
-    temp2->prev = temp1;
+    Node* head = NULL;
+    head = insertEnd(head, 10);
+    head = insertEnd(head, 20);
+    head = insertEnd(head, 30);
+    head = insertEnd(head, 40);
+    head = insertEnd(head, 50);
     traverse(head);
+    head = reverseDLL(head);
+    traverse(head);
+
+    Node* temp = NULL;
+    temp = reverseDLL(temp);
+    traverse(temp);
+    temp = insertEnd(temp, 10);
+    temp = reverseDLL(temp);
+    traverse(temp);
 }
