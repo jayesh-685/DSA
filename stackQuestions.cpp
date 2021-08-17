5b40a29a66df3edb96651823d62f58d194604f09
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-08-17 22:04:48 +0530

    two stacks

--- stackQuestions.cpp
+++ stackQuestions.cpp
@@ -23,15 +23,67 @@ bool balancedParenthesis (const string &s) {
     return false;
 }
 
+// implement 2 stacks in one array
+// we can start one stack from start of array and other one from end of array
+
+struct twoStacks {
+    int* arr;
+    int top1, top2;
+    int cap;
+
+    twoStacks (int x) {
+        arr = new int [x];
+        cap = x;
+        top1 = -1;
+        top2 = x;
+    }
+
+    void push1 (int n) {
+        if (top1 < top2-1) {
+            top1++;
+            arr[top1] = n;
+        } else 
+            cout << "Overflow" << endl;
+    }
+
+    void push2 (int n) {
+        if (top1 < top2-1) {
+            top2--;
+            arr[top2] = n;
+        } else 
+            cout << "Overflow" << endl;
+    }
+
+    int pop1 () {
+        if (top1 == -1)
+            return INT_MIN;
+        else {
+            int temp = arr[top1];
+            top1--;
+            return temp;
+        }
+    }
+
+    int pop2 () {
+        if (top2 == cap)
+            return INT_MAX;
+        else {
+            int temp = arr[top2];
+            top2++;
+            return temp;
+        }
+    }
+};
+
 int main () {
-    string s;
+    /* string s;
     cin >> s;
-    cout << balancedParenthesis(s) << endl;
-    /* stack <char> s;
-    s.push('a');
-    cout << s.top() << endl;
-    string str = "abcd";
-    cout << str.length() << endl;
-    if (str[0] == s.top())
-        cout << true << endl; */
+    cout << balancedParenthesis(s) << endl; */
+
+    twoStacks s (10);
+    s.push1(5);
+    s.push2(50);
+    cout << s.pop1() << " " << s.pop2() << endl;
+    
+    return 0;
 }
