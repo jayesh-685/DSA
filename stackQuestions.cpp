6a922243be9270310da7f09bdafd69dbbdb3cfc2
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-08-18 23:49:29 +0530

    stock span

--- stackQuestions.cpp
+++ stackQuestions.cpp
@@ -75,15 +75,80 @@ struct twoStacks {
     }
 };
 
+// implement k stacks in an array
+// we use three arrays, one to store elements, one to store prev of each element and one to store top of each head. We store index of free space in a separate variable
+// each stack might not be contiguous, like we may have 3 elements of stack 0 then 2 elements of stack 2 and two more elements of stack 0
+
+struct kStacks {
+    int* arr;   // array to store elements
+    int* prev;  // to store index of prev of each element
+    int* top;   // to store top of each stack
+    int freeTop;    // to keep track of free stack
+    int k, cap;     // no of stacks and capacity
+
+    kStacks (int stacks, int n) {
+        freeTop = 0;
+        k = stacks;
+        cap = n;
+        arr = new int [n];
+        prev = new int [n];
+        // we inititalize it this way so that is stores the index of next element and we can use it to update freetop
+        for (int i=0; i<n-1; i++) {
+            prev[i] = i+1;
+        }
+        prev[n-1] = -1;
+        top = new int [stacks];
+        for (int i=0; i<k; i++)
+            top[i] = -1;
+    }
+
+    void push (int x, int sn) {
+        int i = freeTop;
+        arr[i] = x;
+        freeTop = prev[i];
+        prev[i] = top[sn];
+        top[sn] = i;
+    }
+
+    int pop (int sn) {
+        int i = top[sn];
+        top[sn] = prev[i];
+        prev[i] = freeTop;
+        freeTop = i;
+        return arr[i];
+    }
+}; 
+
+// given the stock price on each day, you have to calculate span for each day which is the no of days stock price was less or equal including that day
+void stockSpan (int arr[], int n) {
+    for (int i=0; i<n; i++) {
+        int spanCount = 1;
+        for (int j=i-1; j>=0 && arr[j] <= arr[i]; j--) 
+            spanCount++;
+        cout << spanCount << " ";
+    }
+}
+
 int main () {
     /* string s;
     cin >> s;
     cout << balancedParenthesis(s) << endl; */
 
-    twoStacks s (10);
+    /* twoStacks s (10);
     s.push1(5);
     s.push2(50);
-    cout << s.pop1() << " " << s.pop2() << endl;
+    cout << s.pop1() << " " << s.pop2() << endl; */
+
+    /* kStacks s (3, 10);
+    s.push(5, 0);
+    s.push(10, 0);
+    s.push(15, 1);
+    cout << s.pop(0) << endl;
+    cout << s.pop(1) << endl;
+    cout << s.pop(0) << endl; */
+
+    int arr[] = {13, 15, 12, 14, 16, 8, 6, 4, 10, 30};
+    stockSpan(arr, 10);
     
     return 0;
 }
--- test2.cpp
+++ test2.cpp
@@ -1,101 +1,50 @@
-// { Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 
- // } Driver Code Ends
-//User function Template for C++
-
-
-//Function to push an element into the stack.
-void insert(stack<int> &s,int x)
-{
-    //Your code here
-    s.push(x);
-    
-}
-
-//Function to remove top element from stack.
-void remove(stack<int> &s)
-{
-    s.pop();
-    //Your code here
-}
-
-//Function to print the top element of stack.
-void headOf_Stack(stack<int> &s)
-{
-    int x=s.top();//Your code here
-    cout<<x<<" "<<endl; 
-}
-
-//Function to search an element in the stack.
-bool find(stack<int> s, int val)
-{
-    bool exists=false;
-    
-    //Your code here
-    while (!s.empty()) {
-        if (s.top() == val) {
-            exists = true;
-            break;
-        }
-        s.pop();
-    }
-    
-    if(exists==true){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-// { Driver Code Starts.
-
 int main() {
-	int testcases;
-	cin>>testcases;
-	while(testcases--)
-	{
-	    stack<int> s;
-	    int q;
-	    cin>>q;
-	    while(q--){
-	        char ch;
-	        cin>>ch;
-	        
-	        if(ch=='i')
-	        {
-	            int x;
-	            cin>>x;
-	            
-	            insert(s,x);
-	            
-	        }
-	        else if(ch=='r')
-	        {
-	            remove(s);
-	        }
-	        else if(ch=='h')
-	        {
-	            headOf_Stack(s);
-	        }
-	        else if(ch=='f')
-	        {
-	            int x;
-	            cin>>x;
-	            if(find(s,x))
-	            cout << "Yes";
-	            else cout << "No";
-	            cout << endl;
-	        }
-	        
-	    }
+	// your code goes here
+	int T;
+	cin >> T;
+	while (T--) {
+	    int N, M, Q;
+		cin >> N >> M >> Q;
+		int inBus = 0;
+		int arr[M+1] = {0};
+		char action, temp;
+		int passenger;
+		bool consistent = true;
+		for (int k=0; k<Q; k++) {
+			if (inBus > M) {
+				consistent = false;
+				break;
+			}
+			cin >> action;
+			cin.sync();
+			cin >> passenger;
+			if (action == '+') {
+				if (arr[passenger] == 1) {
+					cout << "Plus if" << endl;
+					consistent = false;
+					break;
+				} else {
+					inBus++;
+					arr[passenger]++;
+				}
+			} else {
+				if (arr[passenger] == 0) {
+					cout << "Minus if" << endl;
+					consistent = false;
+					break;
+				} else {
+					inBus--;
+					arr[passenger]--;
+				}
+			}
+		}
+		if (consistent)
+			cout << "Consistent" << endl;
+		else
+			cout << "Inconsistent" << endl;
 	}
 	return 0;
 }
-
-
-  // } Driver Code Ends
