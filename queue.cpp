1a6e6a4b309b649228afbcdff17f43e2183a29a0
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-09-30 20:12:24 +0530

    level order traversal using queue

--- queue.cpp
+++ queue.cpp
@@ -1,6 +1,8 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+/* A Queue is a linear structure which follows a particular order in which the operations are performed. The order is First In First Out (FIFO). The difference between stacks and queues is in removing. In a stack we remove the item the most recently added; in a queue, we remove the item the least recently added */
+
 /* struct myQueue {
     int* arr;
     int size, cap;
