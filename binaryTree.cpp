41473488518ec2f52a8ea05f3985c85a5dab3dac
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-10-11 21:41:55 +0530

    max width of binary tree

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -16,11 +16,13 @@ using namespace std;
 
 // binary tree implementation
 
-struct Node {
+struct Node
+{
     int key;
-    Node* left;
-    Node* right;
-    Node (int k) {
+    Node *left;
+    Node *right;
+    Node(int k)
+    {
         key = k;
         left = right = NULL;
     }
@@ -35,35 +37,39 @@ Inorder (Left, Root, Right)
 Preorder (Root, Left, Right)
 Postorder (Left, Right, Root)
 
-*/  
+*/
 
-void inorderTraverse (Node* root) {
-    if (root == NULL) 
+void inorderTraverse(Node *root)
+{
+    if (root == NULL)
         return;
     inorderTraverse(root->left);
     cout << root->key << " ";
-    inorderTraverse(root->right);   
+    inorderTraverse(root->right);
 }
 
-void preorderTraverse (Node* root) {
-    if (root == NULL) 
+void preorderTraverse(Node *root)
+{
+    if (root == NULL)
         return;
     cout << root->key << " ";
     preorderTraverse(root->left);
-    preorderTraverse(root->right);   
+    preorderTraverse(root->right);
 }
 
-void postorderTraverse (Node* root) {
-    if (root == NULL) 
+void postorderTraverse(Node *root)
+{
+    if (root == NULL)
         return;
     postorderTraverse(root->left);
-    postorderTraverse(root->right); 
+    postorderTraverse(root->right);
     cout << root->key << " ";
 }
 
 // calculate height of binary tree
-// call function for left part, then for right part find max of them and return adding one to them  
-int getHeight (Node* root) {
+// call function for left part, then for right part find max of them and return adding one to them
+int getHeight(Node *root)
+{
     if (root == NULL)
         return 0;
     else
@@ -73,15 +79,19 @@ int getHeight (Node* root) {
 
 // print nodes at distance k
 
-void printNodesK (Node* root, int k) {
+void printNodesK(Node *root, int k)
+{
     if (root == NULL)
         return;
-    if (k == 0) {
+    if (k == 0)
+    {
         cout << root->key << " ";
         return;
-    } else {
-        printNodesK(root->left, k-1);
-        printNodesK(root->right, k-1);
+    }
+    else
+    {
+        printNodesK(root->left, k - 1);
+        printNodesK(root->right, k - 1);
     }
 }
 // time complexity is O(n) and aux space required is O(h) where h is height of tree
@@ -94,7 +104,7 @@ void printNodesK (Node* root, int k) {
 // optimised way is too use a queue. Push the root, them pop and print it and then push its child nodes (if they exists) and continue it till the queue becomes empty;
 
 // time complexity is theta(n) because we enqueue and dequeue each node exactly once and it takes theta(1) for both these operations
-// auxillary space: in queue we will have at most one level at a time so auxillary space required is theta(w) where w is width of binary tree 
+// auxillary space: in queue we will have at most one level at a time so auxillary space required is theta(w) where w is width of binary tree
 
 // all in one line
 /* void levelOrderTraversal (Node* root) {
@@ -118,18 +128,21 @@ void printNodesK (Node* root, int k) {
 */
 
 // line by line
-void levelOrderTraversal (Node* root) {
+void levelOrderTraversal(Node *root)
+{
     if (root == NULL)
         return;
 
-    queue <Node*> q;
+    queue<Node *> q;
     q.push(root);
     q.push(NULL);
 
-    while (q.size() > 1) {
-        Node* curr = q.front();
+    while (q.size() > 1)
+    {
+        Node *curr = q.front();
         q.pop();
-        if (curr == NULL) {
+        if (curr == NULL)
+        {
             cout << endl;
             q.push(NULL);
             continue;
@@ -179,27 +192,33 @@ void levelOrderTraversal (Node* root) {
 
 // one more way, using two loops
 
-void levelOrderTraversal2 (Node* root) {
+void levelOrderTraversal2(Node *root)
+{
     if (root == NULL)
         return;
 
-    queue <Node*> q;
+    queue<Node *> q;
     q.push(root);
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int count = q.size();
-        for (int i=0; i<count; i++) {
-            Node* curr = q.front();
+        for (int i = 0; i < count; i++)
+        {
+            Node *curr = q.front();
             q.pop();
             cout << curr->key << " ";
-            if (curr->left != NULL) q.push(curr->left);
-            if (curr->right != NULL) q.push(curr->right);
+            if (curr->left != NULL)
+                q.push(curr->left);
+            if (curr->right != NULL)
+                q.push(curr->right);
         }
         cout << endl;
     }
 }
 
-int sizeOfTree (Node* root) {
+int sizeOfTree(Node *root)
+{
     if (root == NULL)
         return 0;
     else
@@ -210,22 +229,26 @@ int sizeOfTree (Node* root) {
 
 // iterative solution
 
-int IsizeOfTree (Node* root) {
+int IsizeOfTree(Node *root)
+{
     if (root == NULL)
         return 0;
-    
-    queue <Node*> q;
+
+    queue<Node *> q;
     q.push(root);
     int size = 1;
 
-    while (!q.empty()) {
-        Node* curr = q.front();
+    while (!q.empty())
+    {
+        Node *curr = q.front();
         q.pop();
-        if (curr->left != NULL) {
+        if (curr->left != NULL)
+        {
             q.push(curr->left);
             size++;
         }
-        if (curr->right != NULL) {
+        if (curr->right != NULL)
+        {
             q.push(curr->right);
             size++;
         }
@@ -234,7 +257,8 @@ int IsizeOfTree (Node* root) {
 }
 // time complexity is O(n) and aux space required is O(w) w = width of binary tree
 
-int getMax (Node* root) {
+int getMax(Node *root)
+{
     if (root == NULL)
         return INT_MIN;
     return max(root->key, max(getMax(root->left), getMax(root->right)));
@@ -248,45 +272,55 @@ int getMax (Node* root) {
 // print the left most element at each level
 
 // iterative solution
-void leftView (Node* root) {
+void leftView(Node *root)
+{
     if (root == NULL)
         return;
-    
-    queue <Node*> q;
+
+    queue<Node *> q;
     q.push(NULL);
     q.push(root);
 
-    while (q.size() > 1) {
-        Node* curr = q.front();
+    while (q.size() > 1)
+    {
+        Node *curr = q.front();
         q.pop();
-        if (curr == NULL) {
+        if (curr == NULL)
+        {
             q.push(NULL);
             cout << q.front()->key << endl;
             continue;
         }
-        if (curr->left != NULL) q.push(curr->left);
-        if (curr->right != NULL) q.push(curr->right);
+        if (curr->left != NULL)
+            q.push(curr->left);
+        if (curr->right != NULL)
+            q.push(curr->right);
     }
 }
 // theta(n) theta(w)
 
 // can also do using 2 loops, just print only when i = 0
-void leftView2 (Node* root) {
+void leftView2(Node *root)
+{
     if (root == NULL)
         return;
 
-    queue <Node*> q;
+    queue<Node *> q;
     q.push(root);
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int count = q.size();
-        for (int i=0; i<count; i++) {
-            Node* curr = q.front();
+        for (int i = 0; i < count; i++)
+        {
+            Node *curr = q.front();
             q.pop();
             if (i == 0)
                 cout << curr->key << endl;
-            if (curr->left != NULL) q.push(curr->left);
-            if (curr->right != NULL) q.push(curr->right);
+            if (curr->left != NULL)
+                q.push(curr->left);
+            if (curr->right != NULL)
+                q.push(curr->right);
         }
     }
 }
@@ -296,31 +330,37 @@ void leftView2 (Node* root) {
 
 int maxLevel = 0;
 
-void printLeftView (Node* root, int currLevel) {
+void printLeftView(Node *root, int currLevel)
+{
     if (root == NULL)
         return;
-        
-    if (currLevel > maxLevel) {
+
+    if (currLevel > maxLevel)
+    {
         cout << root->key << endl;
         maxLevel = currLevel;
     }
-    printLeftView(root->left, currLevel+1);
-    printLeftView(root->right, currLevel+1);
+    printLeftView(root->left, currLevel + 1);
+    printLeftView(root->right, currLevel + 1);
 }
 
-void leftViewRecursive (Node* root) {
+void leftViewRecursive(Node *root)
+{
     printLeftView(root, 1);
 }
 
 // if for every node its value is equal to sum of left and right node (if they exixst) then tree is said to satisfy children sum property
-bool childrenSum (Node* root) {
+bool childrenSum(Node *root)
+{
     if (root == NULL)
         return true;
     if (root->left == NULL && root->right == NULL)
         return true;
     int sum = 0;
-    if (root->left != NULL) sum += root->left->key;
-    if (root->right != NULL)  sum += root->right->key;
+    if (root->left != NULL)
+        sum += root->left->key;
+    if (root->right != NULL)
+        sum += root->right->key;
     return sum == root->key && childrenSum(root->left) && childrenSum(root->right);
 }
 // O(n) O(h)
@@ -329,33 +369,58 @@ bool childrenSum (Node* root) {
 // tree is balanced if the diff in height of left and right sub tree is atmost 1
 // we use the previously done getHeight function
 
-bool isBalanced (Node* root) {
-    if (root == NULL)   return true;
+bool isBalanced(Node *root)
+{
+    if (root == NULL)
+        return true;
     int leftH = getHeight(root->left);
     int rightH = getHeight(root->right);
-    return (abs(leftH-rightH)<=1 && isBalanced(root->left) && isBalanced(root->right));
+    return (abs(leftH - rightH) <= 1 && isBalanced(root->left) && isBalanced(root->right));
 } // O(n^2) because we are calling getHeight function for each node
 
 // can be optimized if we get size of tree and check if it is balanced or not in a single function call, so we can return a pair or we can return -1 if tree is unbalanced and otherwise return height of tree(>=0)
 
-int isBalanced2 (Node* root) {
-    if (root == NULL)  return 0;
+int isBalanced2(Node *root)
+{
+    if (root == NULL)
+        return 0;
     int leftH = isBalanced2(root->left);
     int rightH = isBalanced2(root->right);
-    
-    return (leftH!=-1 && rightH!=-1 && abs(leftH-rightH)<=1) ? max(leftH, rightH)+1 : -1;
+
+    return (leftH != -1 && rightH != -1 && abs(leftH - rightH) <= 1) ? max(leftH, rightH) + 1 : -1;
 }
 // traversing every node exactly once so O(n)
-//
-int main () {
-    Node* root = new Node (30);
-    root->left = new Node (12);
-    root->right = new Node (18);
-    root->left->left = new Node (6);
-    root->left->right = new Node (6);
-    root->right->left = new Node (6);
-    root->right->right = new Node (12);
-    root->left->left->left = new Node (6);
+
+int maxWidth(Node *root)
+{
+    int maxCount = 0;
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        for (int i = 0; i < q.size(); i++)
+        {
+            Node *curr = q.front();
+            q.pop();
+            if (curr->left != NULL)
+                q.push(curr->left);
+            if (curr->right != NULL)
+                q.push(curr->right);
+        }
+        maxCount = (maxCount > q.size()) ? maxCount : q.size();
+    }
+    return maxCount;
+}
+int main()
+{
+    Node *root = new Node(30);
+    root->left = new Node(12);
+    root->right = new Node(18);
+    root->left->left = new Node(6);
+    root->left->right = new Node(6);
+    root->right->left = new Node(6);
+    root->right->right = new Node(12);
+    root->left->left->left = new Node(6);
     //postorderTraverse(root);
     //cout << getHeight(root) << endl;
     // printNodesK(root, 2);
@@ -365,6 +430,6 @@ int main () {
     //cout << getMax(root) << endl;
     //leftViewRecursive(root);
     //cout << childrenSum(root);
-    cout << isBalanced2(root);
-    
+    //cout << isBalanced2(root);
+    cout << maxWidth(root) << endl;
 }
